87d71a2ab7294b0b01606c15009b6f1245757ee7
Ginevra Angelica Marelli <marelliginevra@gmail.com>
2026-04-01 11:32:38 +0200

    inizio punto 1

--- utilities/portfolio_optimization.py
+++ utilities/portfolio_optimization.py
@@ -39,9 +39,11 @@ def minimum_variance_portfolio(cov_matrix: np.ndarray) -> np.ndarray:
 
     n = cov_matrix.shape[0]
     ones_vec = np.ones((n, 1))
+    one_vec_trans = np.ones((1,n))
 
-    min_var_ptf_numerator = None  # !!! COMPLETE AS APPROPRIATE !!!
-    min_var_ptf_weights = None  # !!! COMPLETE AS APPROPRIATE !!!
+    min_var_ptf_numerator = (cov_matrix @ ones_vec)  # !!! COMPLETE AS APPROPRIATE !!!
+    min_var_ptf_den = one_vec_trans @ cov_matrix @ ones_vec  # !!! COMPLETE AS APPROPRIATE !!!
+    min_var_ptf_weights = min_var_ptf_numerator / min_var_ptf_den
 
     return min_var_ptf_weights.flatten()
 
@@ -101,6 +103,13 @@ def mean_variance_portfolio(
             f"risk_aversion must be strictly positive, got {risk_aversion}"
         )
 
-    mean_var_ptf_weights = None  # !!! COMPLETE AS APPROPRIATE !!!
+    n = cov_matrix.shape[0]
+    ones_vec = np.ones((n, 1))
+    one_vec_trans = np.ones((1,n))
+
+    A = one_vec_trans @ np.linalg.inv(cov_matrix) @ ones_vec
+    B = one_vec_trans @ np.linalg.inv(cov_matrix) @ expected_returns
+    mean_var_ptf_weights = (1/risk_aversion * np.linalg.inv(cov_matrix) @ expected_returns -
+                            ((B-risk_aversion)/(risk_aversion*A)) *(np.linalg.inv(cov_matrix)@ ones_vec) ) # !!! COMPLETE AS APPROPRIATE !!!
 
     return mean_var_ptf_weights.flatten()
